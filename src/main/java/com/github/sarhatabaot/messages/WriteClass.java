fe4d9f9e2e106a2ef3c9108c62aabe9d11fc1574
Omer Oreg <sarhatabaot1@gmail.com>
2023-01-17 13:18:23 +0200

    Removed unused code

--- src/main/java/com/github/sarhatabaot/messages/WriteClass.java
+++ src/main/java/com/github/sarhatabaot/messages/WriteClass.java
@@ -7,11 +7,9 @@ import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.Writer;
 import java.util.Map;
 import java.util.Set;
 
@@ -25,20 +23,14 @@ public abstract class WriteClass<T> {
     protected final String privateConstructor;
     protected final boolean overwriteClasses;
     
-    public WriteClass(final String targetPackage, final String basePath, final String privateConstructor, final boolean overwriteClasses) {
+    protected WriteClass(final String targetPackage, final String basePath, final String privateConstructor, final boolean overwriteClasses) {
         this.targetPackage = targetPackage;
         this.basePath = basePath;
         this.privateConstructor = privateConstructor;
         this.overwriteClasses = overwriteClasses;
     }
     
-    protected void writePrivateUtilConstructor(@NotNull Writer fileWriter, String parentClassName) throws IOException {
-        fileWriter.write("private " + parentClassName + " (){");
-        fileWriter.write("throw new UnsupportedOperationException(" + privateConstructor + ");");
-        fileWriter.write("}");
-    }
-    
-    private String arrayAsArrayInit(final String @NotNull [] values) {
+    private @NotNull String arrayAsArrayInit(final String @NotNull [] values) {
         StringBuilder stringBuilder = new StringBuilder();
         for (String value : values) {
             stringBuilder
@@ -51,25 +43,12 @@ public abstract class WriteClass<T> {
         return stringBuilder.toString();
     }
     
-    /**
-     * @return Returns a wrapped string if the class is a string.
-     */
-    private String getWrapped(@NotNull TypeKeyValue value) {
-        if (value.getClazz().equals(String.class)) {
-            return '"' + value.getValue() + '"';
-        }
-        return value.getValue();
-    }
-    
-    @Contract(pure = true)
-    private @NotNull String getFinalVariableName(final String typeName, final String variableName) {
-        return String.format("public static final %s %s", typeName, variableName);
-    }
     
     protected @NotNull String getPathFromPackage() {
         return String.join(File.separator, targetPackage.split("\\."));
     }
     
+    //Entry Point
     public void createJavaClass(@NotNull File sourceFile) throws IOException {
         final String parentFileName = Util.getAsFileName(sourceFile.getName()).replace(getFileType().getExtension(), ".java");
         final String parentClassName = parentFileName.replace(".java", "");
@@ -84,6 +63,11 @@ public abstract class WriteClass<T> {
         }
     }
     
+    @Contract(pure = true)
+    private @NotNull String getUnsupportedOperationMessage() {
+        return "throw new UnsupportedOperationException(" + privateConstructor + ");";
+    }
+    
     private @NotNull JavaClassSource createSourceClass(@NotNull File sourceFile, final String parentClassName) throws IOException {
         final JavaClassSource parentClass = Roaster.create(JavaClassSource.class)
             .setPackage(targetPackage)
@@ -93,7 +77,7 @@ public abstract class WriteClass<T> {
         parentClass.addMethod()
             .setConstructor(true)
             .setPrivate()
-            .setBody("throw new UnsupportedOperationException(" + privateConstructor + ");");
+            .setBody(getUnsupportedOperationMessage());
         
         for (Map.Entry<String, T> entrySet : getRootEntrySet(sourceFile)) {
             writeValue(parentClass, entrySet);
@@ -123,7 +107,7 @@ public abstract class WriteClass<T> {
         innerClass.addMethod()
             .setConstructor(true)
             .setPrivate()
-            .setBody("throw new UnsupportedOperationException(" + privateConstructor + ");");
+            .setBody(getUnsupportedOperationMessage());
         
         
         for (Map.Entry<String, T> elementSet : getEntrySetFromValue(entrySet.getValue())) {
@@ -161,30 +145,6 @@ public abstract class WriteClass<T> {
             .setStringInitializer(arrayAsArrayInit(values));
     }
     
-    
-    private void writeJavaClass(@NotNull File sourceFile, final String parentClassName, final File outputFile) {
-        try (Writer fileWriter = new BufferedWriter(new FileWriter(outputFile, !overwriteClasses))) {
-            fileWriter.write("package " + targetPackage + ";");
-            fileWriter.write("\n");
-            fileWriter.write("public final class " + parentClassName + " ");
-            fileWriter.write("{");
-            fileWriter.write("\n");
-            
-            for (Map.Entry<String, T> entrySet : getRootEntrySet(sourceFile)) {
-                writeValue(fileWriter, entrySet);
-            }
-            //json end
-            fileWriter.write("\n");
-            if (privateConstructor != null && !privateConstructor.isEmpty()) {
-                writePrivateUtilConstructor(fileWriter, parentClassName);
-            }
-            fileWriter.write("}");
-            
-            logger.info(String.format("Created class: %s for file: %s with type: %s", parentClassName, sourceFile.getName(), getFileType().name()));
-        } catch (IOException e) {
-            logger.error(e.getMessage(), e);
-        }
-    }
     
     private @NotNull String translateToFieldKey(final @NotNull String key) {
         if (key.contains("-")) {
